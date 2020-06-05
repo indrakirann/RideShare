c32b03c179110556342e6d5faa5041d727dd2222
Indra <indran@netmatrixsolutions.com>
2020-06-05 14:11:34 -0500

    example

--- src/controller/SignupController.java
+++ src/controller/SignupController.java
@@ -7,7 +7,7 @@ public class SignupController {
 	public User createUser(String firstname, String lastname, String email, String password) {
 		// check existing email, ensure no same users are being added
 		//create user in db
-		User u = new User(firstname, lastname, email, password);
+		User u = new User(firstname, lastname, email.toLowerCase(), password);
 
 		// save u in db
 
