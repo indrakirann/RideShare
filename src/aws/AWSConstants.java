f9414431df1912c95dab12dd3c5548d12b42c03f
Mahek Maheshwari <mahek.maheshwari@berkeley.edu>
2020-06-16 14:14:21 -0500

    final

---
+++ src/aws/AWSConstants.java
@@ -0,0 +1,24 @@
+package com.aws.s3.constants;
+
+public class AWSConstants {
+	
+	public static String ACCESS_KEY_ID = "AKIAWEZ37ZAHYWLG66FE";
+	
+	public static String ACCESS_SEC_KEY = "ACCESS_SEC_KEY";
+	
+	public static String BUCKET_NAME = "amazoncodedeploybucket";
+	
+	public static String FOLDER_NAME = "storeFiles";
+	
+	public static String FILE_NAME = "Invoice_May.pdf";
+	
+	public static String FILE_PATH = "C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\Invoice_May.pdf";
+	
+	public static String BUCKET_FILE_PATH = "storeFiles/Invoice_May.pdf";
+	
+	public static String LOCAL_DOWNLOAD_PATH = "C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\Invoice_May_new.pdf";
+	
+	public static final String SUFFIX = "/";
+	
+
+}
